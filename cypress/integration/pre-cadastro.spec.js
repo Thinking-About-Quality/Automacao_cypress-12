7e40071f2c94015f6f72491458689fc75ac2a60c
Paula <paulasenna7@gmail.com>
2022-01-27 15:59:24 -0300

    Exercicio

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -1,25 +1,30 @@
-///<reference types="cypress" />
+
 var faker = require('faker');
 describe('Funcionalidade Pré Cadastro', () => {
-beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/')
-});
-afterEach(() => {
-    //cy.screenshot()
-});
+    beforeEach(() => {
+        cy.visit('minha-conta/')
+    });
+    
     it('Deve completar o pré cadastro com sucesso', () => {
-        let nomeFaker=(faker.name.firstName())
-        let sobrenomeFaker=(faker.name.lastName())
-        let emailfaker=(faker.internet.email(nomeFaker))
+        let nomeFaker = (faker.name.firstName())
+        let sobrenomeFaker = (faker.name.lastName())
+        let emailfaker = (faker.internet.email(nomeFaker))
+
         cy.get('#reg_email').type(emailfaker)
         cy.get('#reg_password').type('!teste@teste$')
         cy.get(':nth-child(4) > .button').click()
+
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
         cy.get('#account_first_name').type(nomeFaker)
         cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
-        cy.get('.woocommerce-message').should('contain','Detalhes da conta modificados com sucesso.')
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
 
     });
-    
+    it('Deve completar o pré-cadastro com sucesso usando comando customizados', () => {
+        let emailfaker2 = faker.internet.email()
+        cy.preCadastro(emailfaker2, 'senha@@intercalado', 'joao', 'Silva')
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+    });
+
 });
--- cypress/integration/produto.spec.js
+++ cypress/integration/produto.spec.js
@@ -14,14 +14,22 @@ describe('Funcionalidade Página de Produtos', () => {
     });
     it('Deve adicionar o produto ao carrinho', () => {
         var quantidade = 10
+
         cy.get('[class="product-block grid"]')
             .contains('Ariel Roll Sleeve Sweatshirt').click()
         cy.get('.button-variable-item-M').click()
         cy.get('.button-variable-item-Purple').click()
         cy.get('.input-text').clear().type(quantidade)
         cy.get('.single_add_to_cart_button').click()
+
         cy.get('.dropdown-toggle > .mini-cart-items').should('contain',quantidade )
         cy.get('.woocommerce-message').should('contain',quantidade+ ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho')
 
+    });
+    it('Deve adicionar produtos ao carrinho- Usando Comando customizado', () => {
+        cy.addProdutos('Aero Daily Fitness Tee','M','Black',2)
+    });
+    it.only('Deve adicionar produtos ao carrinho- Usando Comando customizado', () => {
+        cy.addProdutos('Ariel Roll Sleeve Sweatshirt','XS','Red',5)
     });
 })
